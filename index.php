b05a4ad8542927522beccc5a87bf9aca375dc98b
Diana Kleban <dik548@g.harvard.edu>
2017-09-16 20:31:14 -0500

    Added index and styles

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Contact Us</title>
+    <meta name="description" content="Diana Teacher"/>
+    <meta name="keywords" content="HTML,CSS,Language,Learn,Russian,English,West Hartford,Diana,Kleban,Teacher" />
+    <meta name="author" content="Diana Kleban">
+    <meta name="HandHeldFriendly" content="true">
+    <meta name="MobileOptimized" content="320">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/contactus.css" type="text/css" />
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <h1 id="imgHere">
+                Language School</h1>
+            <hr id="tophr" />
+        </header>
+        <aside>
+                <img src="images/DianaTeacher.jpg" id="imgTeacher" title="Diana Kleban" alt="" />
+        </aside>
+        <article>
+            <section id="teacher">
+                <h2 id="teacherNm">
+                    Contact Us</h2>
+
+            </section>
+            <section id="info">
+
+            </section>
+        </article>
+        <footer>
+            <p>
+                <strong>Diana Kleban</strong> West Hartford, CT
+                <br />
+                <a href="mailto:dik548@g.harvard.com">dik548@g.harvard.com</a>
+                <br />
+                Copyright @2017. All Rights Reserved.
+            </p>
+        </footer>
+    </div>
+</body>
+</html>
