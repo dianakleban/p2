6c2a1194d99c4916ba73b1b7a6852eda002039a2
Diana Kleban <dik548@g.harvard.edu>
2017-09-17 20:08:52 -0500

    Added logic and image

---
+++ formLogic.php
@@ -0,0 +1,42 @@
+<?php
+require('helpers.php');
+
+if (isset($_POST['name'])) {
+    $contactNm = $_POST['name'];
+} else {
+    $contactNm = '';
+}
+
+if (isset($_POST['email'])) {
+    $contactEmail = $_POST['email'];
+} else {
+    $contactEmail = '';
+}
+
+if (isset($_POST['comments'])) {
+    $comments = $_POST['comments'];
+} else {
+    $comments = '';
+}
+
+$lang = '';
+$results = '';
+
+if (isset($_POST['lang'])) {
+    $lang = $_POST['lang'];
+
+    if ($lang == 'choose') {
+        //$alertType = 'alert-danger';
+        $results = 'Please choose your language.';
+    } else {
+        //$alertType = 'alert-info';
+        if($contactNm != '') {
+          $results = $contactNm . ', ';
+        }
+        $results .= 'Great Choice of Language: ' . $lang . '!';
+
+        if($comments != '') {
+          $results .= ' Thank you for your comments: ' . '"' . $comments . '"';
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,14 +1,16 @@
+<?php require('formLogic.php'); ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
     <title>Contact Us</title>
-    <meta name="description" content="Diana Teacher"/>
-    <meta name="keywords" content="HTML,CSS,Language,Learn,Russian,English,West Hartford,Diana,Kleban,Teacher" />
+    <meta name="description" content="Contact Us"/>
+    <meta name="keywords" content="HTML,CSS,Language,Learn,Diana,Kleban,Teacher" />
     <meta name="author" content="Diana Kleban">
     <meta name="HandHeldFriendly" content="true">
     <meta name="MobileOptimized" content="320">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>-->
     <link rel="stylesheet" href="styles/contactUs.css" type="text/css" />
 </head>
 <body>
@@ -16,18 +18,49 @@
         <header>
             <h1 id="imgHere">
                 Language School</h1>
-            <hr id="tophr" />
+                <hr id="tophr" />
         </header>
         <aside>
-                <img src="images/DianaTeacher.jpg" id="imgTeacher" title="Diana Kleban" alt="" />
+                <img src="images/phone.jpg" id="imgId" title="Diana Kleban" alt="" />
         </aside>
         <article>
-            <section id="teacher">
-                <h2 id="teacherNm">
-                    Contact Us</h2>
+            <section id="myform">
 
-            </section>
-            <section id="info">
+              <form id='myForm' method='POST' action='index.php'>
+                  <fieldset>
+                    <legend>Contact Us</legend>
+                    <p>
+                        <label for="name" class="category">Your Name:</label>
+                        <input name="name" type="text" size="32" id="name" value='<?=sanitize($contactNm)?>' autofocus/> <span class="reqMsg" id="nameSpn"> (Required)</span>
+                    </p>
+
+                    <p>
+                        <label for="email" class="category"> Your Email:</label>
+                        <input name="email" type="email" id="email" size="32" value='<?=sanitize($contactEmail)?>'/><span class="reqMsg" id="emailSpn"> (Required)</span>
+                    </p>
+                    <p>
+                        <label for="langId" class="category">Language to Learn:</label>
+                        <select name="lang" id="langId">
+                            <option value='choose'>Select</option>
+                            <option value='Russian' <?php if ($lang == 'Russian') echo 'SELECTED'?>>Russian</option>
+                            <option value='English' <?php if ($lang == 'English') echo 'SELECTED'?>>English</option>
+                            <option value='French' <?php if ($lang == 'French') echo 'SELECTED'?>>French</option>
+                            <option value='Italian' <?php if ($lang == 'Italian') echo 'SELECTED'?>>Italian</option>
+                        </select>
+                    </p>
+                    <p>
+                        <label for="comments" class="category">Comments: </label><br />
+                        <textarea name="comments" cols="60" rows="6" id="comments" placeholder="Any additional instructions"><?=sanitize($comments)?></textarea>
+                    </p>
+
+                    <input type="submit" value="Submit" />
+
+                    <?php if ($_POST) : ?>
+                      <div class="alert">
+                        Welcome to our Language School! <?=sanitize($results)?>
+                      </div>
+                    <?php endif; ?>
+                </fieldset>
 
             </section>
         </article>
