c92a6e2c1ef3ccbc75f67b4ed108dc1f31635cea
Diana Kleban <dik548@g.harvard.edu>
2017-09-27 13:13:02 -0500

    Moved some logic to display file

--- formLogic.php
+++ formLogic.php
@@ -6,31 +6,19 @@ use DWA\Form;
 
 $form = new Form($_POST);
 
-$results = '';
 $lang = '';
 
 if ($form->isSubmitted()) {
 
-    $name = $form->get('name', '');
-    $email = $form->get('email', '');
-    $comments = $form->get('comments', '');
-    $lang = $form->get('lang', '');
+  $name = $form->get('name', '');
+  $email = $form->get('email', '');
+  $comments = $form->get('comments', '');
+  $lang = $form->get('lang', '');
 
-    $errors = $form->validate([
-            'name' => 'required|alpha',
-            'email' => 'required|email',
-            'lang'=> 'required'
-    ]);
+  $errors = $form->validate([
+    'name' => 'required|alpha',
+    'email' => 'required|email',
+    'lang'=> 'required'
+  ]);
 
-  //if there is no errors
-  if (empty($errors)) {
-
-    $results = sanitize($name) . ', ';
-    $results .= 'Great Choice of Language: ' . sanitize($lang) . '! ';
-
-    if($comments != '') {
-      $results .= 'Thank you for your comments: ' . '"' . sanitize($comments) . '"';
-    }
-
-  }
-}//END OF submit
+}//end of submit
--- index.php
+++ index.php
@@ -16,81 +16,86 @@
   <div id="wrapper">
     <header>
       <h1 id="imgHere">Language School</h1>
-        <hr id="tophr" />
+      <hr id="tophr" />
     </header>
-      <aside>
-        <img src="images/phone.jpg" id="imgId" title="Contact Us" alt="Contact Us" />
-      </aside>
-      <article>
-          <h2> We can not wait to hear from you </h2>
+    <aside>
+      <img src="images/phone.jpg" id="imgId" title="Contact Us" alt="Contact Us" />
+    </aside>
+    <article>
+      <h2> We can not wait to hear from you </h2>
 
-          <form method='POST' action='index.php'>
+      <form method='POST' action='index.php'>
 
-            <fieldset>
-              <legend>Contact Us</legend>
-              <p>
-                <label for="name" class="category">Your Name:</label>
-                <input name="name" type="text" size="32" id="name" value='<?=$form->prefill('name', '')?>' autofocus>
-                <span class="reqMsg" id="nameSpn"> (Required)</span>
-              </p>
+        <fieldset>
+          <legend>Contact Us</legend>
+          <p>
+            <label for="name" class="category">Your Name:</label>
+            <input name="name" type="text" size="32" id="name" value='<?=$form->prefill('name', '')?>' autofocus>
+            <span class="reqMsg" id="nameSpn"> (Required)</span>
+          </p>
 
-              <p>
-                <label for="email" class="category"> Your Email:</label>
-                <input name="email" type="email" id="email" size="32" value='<?=$form->prefill('email', '')?>'>
-                <span class="reqMsg" id="emailSpn"> (Required)</span>
-              </p>
+          <p>
+            <label for="email" class="category"> Your Email:</label>
+            <input name="email" type="email" id="email" size="32" value='<?=$form->prefill('email', '')?>'>
+            <span class="reqMsg" id="emailSpn"> (Required)</span>
+          </p>
 
-              <p>
-                <label for="langId" class="category">Language to Learn:</label>
-                <select name="lang" id="langId">
-                  <option value=''>Select</option>
-                  <option value='Russian' <?php if ($lang == 'Russian') echo 'SELECTED'?>>Russian</option>
-                  <option value='English' <?php if ($lang == 'English') echo 'SELECTED'?>>English</option>
-                  <option value='French' <?php if ($lang == 'French') echo 'SELECTED'?>>French</option>
-                  <option value='Italian' <?php if ($lang == 'Italian') echo 'SELECTED'?>>Italian</option>
-                </select><span class="reqMsg" id="langSpn"> (Required)</span>
-              </p>
+          <p>
+            <label for="langId" class="category">Language to Learn:</label>
+            <select name="lang" id="langId">
+              <option value=''>Select</option>
+              <option value='Russian' <?php if ($lang == 'Russian') echo 'SELECTED'?>>Russian</option>
+              <option value='English' <?php if ($lang == 'English') echo 'SELECTED'?>>English</option>
+              <option value='French' <?php if ($lang == 'French') echo 'SELECTED'?>>French</option>
+              <option value='Italian' <?php if ($lang == 'Italian') echo 'SELECTED'?>>Italian</option>
+            </select><span class="reqMsg" id="langSpn"> (Required)</span>
+          </p>
 
-              <p>
-                <label for="comments" class="category">Comments: </label><br />
-                <textarea name="comments" cols="60" rows="6" id="comments" placeholder="Additional Info"><?=$form->prefill('comments', '')?></textarea>
-              </p>
+          <p>
+            <label for="comments" class="category">Comments: </label><br />
+            <textarea name="comments" cols="60" rows="6" id="comments" placeholder="Additional Info"><?=$form->prefill('comments', '')?></textarea>
+          </p>
 
-              <input type="submit" value="Submit" />
+          <input type="submit" value="Submit" />
 
-              <?php if (!empty($errors)) : ?>
-                <div class='alert'>
-                  <ul>
-                    <?php foreach ($errors as $error) : ?>
-                      <li><?=$error?></li>
-                    <?php endforeach; ?>
-                  </ul>
-                </div>
+          <?php if (!empty($errors)) : ?>
+            <div class='alert'>
+              <ul>
+                <?php foreach ($errors as $error) : ?>
+                  <li><?=$error?></li>
+                <?php endforeach; ?>
+              </ul>
+            </div>
+          <?php endif; ?>
+
+          <?php if ($form->isSubmitted() and empty($errors)) : ?>
+
+            <div class="welcome">
+              <p>Welcome to our Language School!</p>
+              <p><span class="yourNm"><?=sanitize($name)?>,</span> Great Choice of Language: <?=$lang?>!</p>
+
+              <?php if(trim($comments) !=''): ?>
+                <p>Thank you for your comments: "<?=sanitize($comments)?>"</p>
               <?php endif; ?>
 
-              <?php if ($form->isSubmitted() and empty($errors)) : ?>
+              <p>We will get back to you soon!</p>
+            </div>
 
-                <div class="welcome">
-                  <p>Welcome to our Language School!</p>
-                  <p><?=$results?></p>
-                  <p>We will get back to you soon!</p>
-                </div>
+          <?php endif; ?>
 
-              <?php endif; ?>
+        </fieldset>
+      </form>
 
-            </fieldset>
-          </form>
-
-      </article>
-      <footer>
-        <p>
-          <strong>Diana Kleban</strong> West Hartford, CT
-          <br />
-          <a href="mailto:dik548@g.harvard.com">dik548@g.harvard.com</a>
-          <br />
-          Copyright @2017. All Rights Reserved.
-        </p>
-      </footer>
-    </div>
-  </body>
-  </html>
+    </article>
+    <footer>
+      <p>
+        <strong>Diana Kleban</strong> West Hartford, CT
+        <br />
+        <a href="mailto:dik548@g.harvard.com">dik548@g.harvard.com</a>
+        <br />
+        Copyright @2017. All Rights Reserved.
+      </p>
+    </footer>
+  </div>
+</body>
+</html>
